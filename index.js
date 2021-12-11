43a0322240ab64dc64913837697fc45332c1d79f
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2021-12-11 21:16:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ module.exports = {
     });
   });
   },
-  checkAuth:(req, res, next, login) => {
+  checkAuth:((login)(req, res, next)) => {
     if (req.isAuthenticated()) return next();
     req.session.backURL = req.url;
     res.redirect(login);
